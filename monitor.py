944612a3f41c12933e40bfba33621d2e5fdc98d7
Harrison <wcg6121@gmail.com>
2014-09-23 16:14:12 +0800

    修正 _get_ip_port方法在获取进程表后，某进程退出造成无法获取此进程的属性而导致程序崩溃

--- monitor.py
+++ monitor.py
@@ -208,11 +208,16 @@ class Monitor(object):
         """
 
         result = []
-        for proc in [i for i in psutil.process_iter() if i.name() == proc_name]:
-            listen = list(sorted([laddr.laddr for laddr in proc.get_connections() if laddr.status == 'LISTEN'])[0])
-            if listen[0] == '0.0.0.0' or listen[0] == '::' or listen[0] == '127.0.0.1' or listen[0] == '':
-                listen[0] = Monitor._get_local_ip()
-            result.append([str(listen[0]), str(listen[1])])
+        try:
+            for proc in psutil.process_iter():
+                if proc.name() == proc_name:
+                    listen = list(sorted([laddr.laddr for laddr in proc.get_connections() if laddr.status == 'LISTEN'])[0])
+                    if listen[0] == '0.0.0.0' or listen[0] == '::' or listen[0] == '127.0.0.1' or listen[0] == '':
+                        listen[0] = Monitor._get_local_ip()
+                    result.append([str(listen[0]), str(listen[1])])
+        except psutil.NoSuchProcess:
+            pass
+
         return result
 
     # def _get_instance_list(self, procname=None, is_discovery=None, discovery_func=None):
