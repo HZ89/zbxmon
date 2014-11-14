3a24bf080d175019ec547677265873f7fe3cb3c0
Justin Ma <kerrigell@gmail.com>
2014-11-14 16:53:51 +0800

    change project name

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from distutils.core import setup
 setup(
     name='zbxmon',
     version='0.1',
-    packages=['zsmc', 'zsmc/lib'],
-    provides=['zsmc'],
+    packages=['zbxmon', 'zbxmon/lib'],
+    provides=['zbxmon'],
     url='https://github.com/damagedcode/zabbix_service_monitor_script',
     license='MIT',
     author='Harrison Zhu && Justin Ma',
@@ -16,7 +16,7 @@ setup(
     description='A script for zabbix agent monitor service, etc. mysql, mongodb, memcache..',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r').read(),
     scripts=[
-        'scripts/zsmc',
+        'scripts/zbxmon',
     ]
 )
 
