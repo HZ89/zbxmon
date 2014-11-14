0765bc98bbc5cf721d19e6f0305304b6db6cb040
Harrison <wcg6121@gmail.com>
2014-11-14 16:28:25 +0800

    finish setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     provides=['zsmc'],
     url='https://github.com/damagedcode/zabbix_service_monitor_script',
     license='MIT',
-    author='HarrisonZhu',
+    author='Harrison Zhu && Justin Ma',
     author_email='wcg6121@gmail.com',
     description='A script for zabbix agent monitor service, etc. mysql, mongodb, memcache..',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r').read(),
